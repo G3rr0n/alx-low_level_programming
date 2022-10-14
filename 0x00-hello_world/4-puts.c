167367aa2d831ff533fd4f9ffb7a9e310788c75d
= <=>
2022-10-13 18:05:03 -0700

    Puts function and betty linting-adding description to main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/* main function is the starting point for all c programs */
-int main (void)
+/**
+ * main - entry point for all c code
+ *
+ *Return: return (0) as exit code
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }

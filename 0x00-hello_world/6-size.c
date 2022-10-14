685cf0e93061453fb8e3e8a331d6d416986155c1
= <=>
2022-10-13 18:42:59 -0700

    Print data type sizes with %d or %zu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-	char char1;
-	int int1;
-	long int l_int;
-	long long int l_l_int;
-	float float1;
+	char char_variable;
+	int integer_variable;
+	long int long_int_var;
+	long long int very_long_int;
+	float float_variable;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(char1));
-	printf("Size of an int: %d byte(s)\n", sizeof(int1));
-	printf("Size of a long int: %d byte(s)\n", sizeof(l_int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(l_l_int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float1));
+	printf("Size of a char: %d byte(s)\n", sizeof(char_variable));
+	printf("Size of an int: %d byte(s)\n", sizeof(integer_variable));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long_int_var));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(very_long_int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float_variable));
 	return (0);
 }

44a76bdba5f9437356aa7b7669dc601f93681474
= <=>
2022-10-14 22:03:51 -0700

    Printing number combo with putchar and while loop

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-	int i;
+	int i = '0';
 
-	for (i = '0'; i <= '9'; i++)
+	while (i <= '9')
 	{
-		if (i < '9')
+		putchar(i);
+
+		if (i != '9')
 		{
-			putchar(i);
 			putchar(',');
+			putchar(' ');
 		}
-		else
-		{
-			putchar(i);
-		}
+
+		i++;
 	}
 	putchar('\n');
 	return (0);

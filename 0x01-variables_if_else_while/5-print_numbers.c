05b8acd5e0f43aa1298831578f3675815d503728
= <=>
2022-10-14 20:25:55 -0700

    Printing numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of all c code
+ *
+ *Return: return 0 to exit code
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}

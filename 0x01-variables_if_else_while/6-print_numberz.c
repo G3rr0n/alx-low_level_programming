ab4b90541a01294569fedcb68ed40893831dbd0f
= <=>
2022-10-14 20:58:08 -0700

    Print 0-9 with putchar only

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of all c code
+ *
+ *Return: return 0 to exit code
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

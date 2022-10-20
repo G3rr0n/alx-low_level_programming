7e9769340f86b3150887e742433a6c1c1aead08c
= <=>
2022-10-19 22:38:59 -0700

    Sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two numbers together.
+ * @a: The first number to be added.
+ * @b: The second number to be added.
+ * Return: the sum of two numbers and exit.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

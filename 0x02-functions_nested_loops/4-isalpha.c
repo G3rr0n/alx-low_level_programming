ab571cab719a862d882860cfc741bca25e97756b
= <=>
2022-10-19 21:22:17 -0700

    Upper case or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha function - Shows 1 if the input is a
+ * else other cases show 0
+ * @c: parameter to check when functio is invoked
+ *Return: 1 for letters, 0 for the rest
+*/
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

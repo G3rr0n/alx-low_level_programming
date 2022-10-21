9a00588b617fd84c9bbf09caf7a0b52ee993ea1e
= <=>
2022-10-21 14:47:29 -0700

    Multiplying two given numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - function to multiply two given numbers
+ * @a: first param
+ * @b: second param
+ *
+ * Return: return 0 to exit code
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@ More functions and Nested Loops Assignments
 
 1. Check for uppercase
 2. Check for digit
+3. Multiplying two numbers

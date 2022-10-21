48699662f975114fa23c810a5e68eec9a92791e2
= <=>
2022-10-21 16:55:55 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints a list of numbers from 0-14
+ *
+ * Return: return 0 to exit code
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i < = 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				putchar('1');
+				putchar(j % 10 + '0');
+		}
+		putchar('\n');
+	}
+}

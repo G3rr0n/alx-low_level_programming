894a1240a441a96c64cecf4fa1127276fbc482c6
= <=>
2022-10-21 19:49:24 -0700

    Debugging the greatest among 3 numbers

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int greatest;
+
+	if (a >= b && a >= c)
+	{
+		greatest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		greatest = b;
+	}
+	else
+	{
+		greatest = c;
+	}
+
+	return (greatest);
+}
+

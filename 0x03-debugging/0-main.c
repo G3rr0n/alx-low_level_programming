bf7088bb217c2cee8cfde845dc30e447dd1a7a1a
= <=>
2022-10-21 19:21:53 -0700

    Debugging postive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point and here for testing
+ *
+ * Return: 0 to exit code
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
+

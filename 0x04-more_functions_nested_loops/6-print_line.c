555cba7c2fbe41c16e095d2d1e1075b1cc0a2735
= <=>
2022-10-21 17:34:38 -0700

    Printing lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ *
+ * @n: param for function (number of lines)
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}

4d00423955f5aa2c7131848afefe89d22b062cb9
= <=>
2022-10-25 15:50:50 -0700

    Pointer function that updates a given value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  reset_to_98 - takes an int pointer and changes value to 98
+ *  @n: pointer parameter
+ *
+ *  Return: return 0 to exit code
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Readme file for 0x05 Assignments
+
+1. function that takes int pointer as param and updates it.

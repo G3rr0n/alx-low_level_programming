b75a3c3794f0141dba76af6ac09c37160b728671
= <=>
2022-10-25 16:29:23 -0700

    Printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a given string in reverse
+ *
+ * @s: parameter string to reverse
+ *
+ * Return: return 0 to exit
+ */
+
+void print_rev(char *s)
+{
+	int forward, reverse, length;
+
+	while (s[forward] != '\0')
+	{
+		forward++;
+	}
+
+	length = forward;
+
+	for (reverse = length - 1; reverse >= 0; reverse--)
+	{
+		_putchar(s[reverse]);
+	}
+
+	_putchar('\n');
+}
+

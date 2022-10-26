fd0c3dce02e843bf81bd963b57a2746a404b1698
= <=>
2022-10-25 17:15:13 -0700

    Printing specified array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an n number of characters in an array
+ * @n: number array items to print;
+ * @a: array to check
+ *
+ * Return: return 0 to exit
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i < 0;i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+
+	printf('\n');
+}
+

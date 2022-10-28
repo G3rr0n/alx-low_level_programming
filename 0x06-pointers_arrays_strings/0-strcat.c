057432bd6f234317e7ece8e403651eaa6f47f75f
= <=>
2022-10-27 17:57:31 -0700

    String concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,12 +18,9 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	j = 0;
-	while (src[j] != '\0')
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
-		j++;
-		i++;
 	}
 
 	dest[i] = '\0';

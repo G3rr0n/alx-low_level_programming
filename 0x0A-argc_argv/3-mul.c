fa99658e35c1a0917954e2a67f98efc1eac204c4
= <=>
2022-11-08 07:24:08 -0800

    multiplying params

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function to return product of two numbers or error
+ * @argc: count of params
+ * @argv: string of params
+ * Return:  exit code
+ */
+
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		product = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", product);
+		return (0);
+	}
+}
+

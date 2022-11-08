48bf6b86ba2716271086eb7389e83cd8481d287c
= <=>
2022-11-08 05:23:08 -0800

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for all c code
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return:  exit from code with 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

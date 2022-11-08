6293f3c6a27e2cd4042fbaecb1f404c994226938
= <=>
2022-11-08 06:02:34 -0800

    Print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function, prints number of args in passe to the func
+ * @argc: number of parmas
+ * @argv: array of params
+ * Return: returns 0 to exit code
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

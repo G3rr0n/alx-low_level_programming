c0babdf0e5d94382f85ca175df53d420c4ead7ce
= <=>
2022-11-10 11:30:30 -0800

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory for an array
+ * with each member size of input bytes
+ *
+ * @nmemb: number of elements
+ * @size: size of the byte of an element
+ *
+ * Return - exit func with required output
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *filler;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+		return (NULL);
+
+	filler = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+		filler[index] = '\0';
+
+	return (mem);
+}

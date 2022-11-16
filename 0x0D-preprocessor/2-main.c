29629192185aaa7de4e547fc5b388eafc073c0fe
= <=>
2022-11-15 20:58:57 -0800

    Preprocessor and macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for all c code, prints name of the file of the program
+ *
+ * Return: return 0 to exit
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
